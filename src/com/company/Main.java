83fb836b9ae52d4e9ba9973e8dc2fb165541bbf1
Richard Sussan <rsussa1@students.towson.edu>
2016-04-26 19:11:23 -0400

    Small test for database connectivity

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,36 @@
 package com.company;
+import java.sql.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+	static final String userName="rsussa1";//put your MySQL user name
+    static final String password="Cosc*q76p";//put your MySQL password
+    private static Connection connection=null;
+    /**
+     * @param args the command line arguments
+     * @throws java.lang.ClassNotFoundException
+     * @throws java.lang.InstantiationException
+     * @throws java.lang.IllegalAccessException
+     * @throws java.sql.SQLException
+     */
+    public static void main(String[] args) throws ClassNotFoundException, InstantiationException, IllegalAccessException, SQLException {
+        
+        Object newInstance;
+        newInstance = Class.forName("com.mysql.jdbc.Driver").newInstance();
+        connection = DriverManager.getConnection("jdbc:mysql://triton.towson.edu:3360/rsussa1db", userName, password);// Please use your database name here
+        PreparedStatement updateStaff;
+        Statement queryStatement = connection.createStatement();
+        updateStaff = null;
+            String querys="select * from rsussa1db.Employee;";
+            ResultSet results = queryStatement.executeQuery(querys);
+            while(results.next())
+            {
+                System.out.print(results.getString("fName"));
+                System.out.print("    ");
+                System.out.print(results.getString("lName"));
+                System.out.println();
+            }
     }
 }
