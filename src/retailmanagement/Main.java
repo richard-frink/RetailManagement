b94b171d723ad92c26239c149464ddfa4f3fce12
Saurabh Khatiwada <khatiwada.saurabh@gmail.com>
2016-05-04 07:22:16 -0400

    Patched up Inventory. Vendors also finished.

--- src/retailmanagement/Main.java
+++ src/retailmanagement/Main.java
@@ -30,14 +30,14 @@ public class Main {
         //PreparedStatement updateStaff;
         Statement queryStatement = connection.createStatement();
         //updateStaff = null;
-        String querys = "SELECT SKU,Name,CurrentStock,RetailPrice,VendorPrice,VendorName FROM rsussa1db.Products WHERE CurrentStock>-1;";
-        ResultSet results = queryStatement.executeQuery(querys);
-        while (results.next()) {
-            System.out.print(results.getString("Name"));
-            System.out.print("    ");
-            System.out.print(results.getString("CurrentStock"));
-            System.out.println();
-        }
+        String querys = "DELETE FROM Vendors WHERE Name='Mountain Dew'";
+        //int results = queryStatement.executeUpdate(querys);
+//        while (results.next()) {
+//            System.out.print(results.getString("Name"));
+//            System.out.print("    ");
+//            System.out.print(results.getString("CurrentStock"));
+//            System.out.println();
+//        }
 //        String name="";
 //        for (int i = 1; i <= 4; i++ ) {
 //            name += " " + results.getString("currentStock");
