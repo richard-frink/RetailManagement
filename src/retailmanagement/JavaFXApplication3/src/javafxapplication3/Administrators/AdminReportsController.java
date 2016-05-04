a42e7842517cf3a0b1639d988ead61b92b7d9ac6
Ted Smith III <tsmith1119s@gmail.com>
2016-05-04 18:02:47 -0400

    Here are my report updates

--- src/retailmanagement/JavaFXApplication3/src/javafxapplication3/Administrators/AdminReportsController.java
+++ src/retailmanagement/JavaFXApplication3/src/javafxapplication3/Administrators/AdminReportsController.java
@@ -8,7 +8,18 @@ package javafxapplication3.Administrators;
 import javafx.fxml.Initializable;
 
 import java.net.URL;
+import java.sql.ResultSet;
 import java.util.ResourceBundle;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.value.ObservableValue;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.util.Callback;
+import javafxapplication3.Runner;
 
 /**
  * FXML Controller class for HomeAdmin
@@ -18,7 +29,8 @@ import java.util.ResourceBundle;
  */
 public class AdminReportsController implements Initializable {
 
-
+@FXML
+private TableView tblReports;
     /**
      * Initializes the controller class.
      */
@@ -29,6 +41,187 @@ public class AdminReportsController implements Initializable {
     }
 
 
+        public void handleNewBtn (ActionEvent event){
+            
+        }
+        public void handleVendBtn (ActionEvent event){
+            populateVendTableView();
+        }
+        public void handleRevBtn (ActionEvent event){
+            populateRevTableView();
+        }
+        public void handleActiveOrderBtn (ActionEvent event){
+            populateOpenOrderTableView();
+        }
+         public void handleStockBtn (ActionEvent event){
+            
+             populateStockTableView();
+        }
+       
+         private void populateStockTableView() {
+        tblReports.getColumns().clear();
+        try {
+            ResultSet rs = Runner.sC.runQuery("SELECT SKU, Name, CurrentStock, RetailPrice FROM rsussa1db.Products where CurrentStock < 10;");
+            ObservableList<ObservableList> data = FXCollections.observableArrayList();
+            for (int i = 0; i < rs.getMetaData().getColumnCount(); i++) {
+                //We are using non property style for making dynamic table
+                final int j = i;
+                TableColumn col = new TableColumn(rs.getMetaData().getColumnName(i + 1));
+                col.setCellValueFactory(new Callback<TableColumn.CellDataFeatures<ObservableList, String>, ObservableValue<String>>() {
+                    public ObservableValue<String> call(TableColumn.CellDataFeatures<ObservableList, String> param) {
+                        return new SimpleStringProperty(param.getValue().get(j).toString());
+                    }
+                });
 
+                tblReports.getColumns().addAll(col);
+            }
+
+            /**
+             * ******************************
+             * Data added to ObservableList *
+             *******************************
+             */
+            while (rs.next()) {
+                //Iterate Row
+                ObservableList<String> row = FXCollections.observableArrayList();
+                for (int i = 1; i <= rs.getMetaData().getColumnCount(); i++) {
+                    //Iterate Column
+                    row.add(rs.getString(i));
+                }
+                System.out.println("Row [1] added " + row);
+                data.add(row);
+
+            }
+
+            //FINALLY ADDED TO TableView
+            tblReports.setItems(data);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+ private void populateOpenOrderTableView() {
+        tblReports.getColumns().clear();
+        try {
+            ResultSet rs = Runner.sC.runQuery("SELECT OrderId, IsActive, OrderDate FROM rsussa1db.Orders where IsActive = 1;");
+            ObservableList<ObservableList> data = FXCollections.observableArrayList();
+            for (int i = 0; i < rs.getMetaData().getColumnCount(); i++) {
+                //We are using non property style for making dynamic table
+                final int j = i;
+                TableColumn col = new TableColumn(rs.getMetaData().getColumnName(i + 1));
+                col.setCellValueFactory(new Callback<TableColumn.CellDataFeatures<ObservableList, String>, ObservableValue<String>>() {
+                    public ObservableValue<String> call(TableColumn.CellDataFeatures<ObservableList, String> param) {
+                        return new SimpleStringProperty(param.getValue().get(j).toString());
+                    }
+                });
+
+                tblReports.getColumns().addAll(col);
+            }
+
+            /**
+             * ******************************
+             * Data added to ObservableList *
+             *******************************
+             */
+            while (rs.next()) {
+                //Iterate Row
+                ObservableList<String> row = FXCollections.observableArrayList();
+                for (int i = 1; i <= rs.getMetaData().getColumnCount(); i++) {
+                    //Iterate Column
+                    row.add(rs.getString(i));
+                }
+                System.out.println("Row [1] added " + row);
+                data.add(row);
+
+            }
+
+            //FINALLY ADDED TO TableView
+            tblReports.setItems(data);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+  private void populateRevTableView() {
+        tblReports.getColumns().clear();
+        try {
+            ResultSet rs = Runner.sC.runQuery("SELECT * FROM rsussa1db.FinancialReports;");
+            ObservableList<ObservableList> data = FXCollections.observableArrayList();
+            for (int i = 0; i < rs.getMetaData().getColumnCount(); i++) {
+                //We are using non property style for making dynamic table
+                final int j = i;
+                TableColumn col = new TableColumn(rs.getMetaData().getColumnName(i + 1));
+                col.setCellValueFactory(new Callback<TableColumn.CellDataFeatures<ObservableList, String>, ObservableValue<String>>() {
+                    public ObservableValue<String> call(TableColumn.CellDataFeatures<ObservableList, String> param) {
+                        return new SimpleStringProperty(param.getValue().get(j).toString());
+                    }
+                });
+
+                tblReports.getColumns().addAll(col);
+            }
+
+            /**
+             * ******************************
+             * Data added to ObservableList *
+             *******************************
+             */
+            while (rs.next()) {
+                //Iterate Row
+                ObservableList<String> row = FXCollections.observableArrayList();
+                for (int i = 1; i <= rs.getMetaData().getColumnCount(); i++) {
+                    //Iterate Column
+                    row.add(rs.getString(i));
+                }
+                System.out.println("Row [1] added " + row);
+                data.add(row);
+
+            }
+
+            //FINALLY ADDED TO TableView
+            tblReports.setItems(data);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+  
+  private void populateVendTableView() {
+        tblReports.getColumns().clear();
+        try {
+            ResultSet rs = Runner.sC.runQuery("SELECT vend.Name, sum(prod.CurrentStock) as 'Stock Bought' FROM rsussa1db.Products as prod,rsussa1db.Vendors as vend  where prod.VendorName = vend.Name Group by prod.VendorName having sum(prod.CurrentStock) > 0 ;");
+            ObservableList<ObservableList> data = FXCollections.observableArrayList();
+            for (int i = 0; i < rs.getMetaData().getColumnCount(); i++) {
+                //We are using non property style for making dynamic table
+                final int j = i;
+                TableColumn col = new TableColumn(rs.getMetaData().getColumnName(i + 1));
+                col.setCellValueFactory(new Callback<TableColumn.CellDataFeatures<ObservableList, String>, ObservableValue<String>>() {
+                    public ObservableValue<String> call(TableColumn.CellDataFeatures<ObservableList, String> param) {
+                        return new SimpleStringProperty(param.getValue().get(j).toString());
+                    }
+                });
+
+                tblReports.getColumns().addAll(col);
+            }
+
+            /**
+             * ******************************
+             * Data added to ObservableList *
+             *******************************
+             */
+            while (rs.next()) {
+                //Iterate Row
+                ObservableList<String> row = FXCollections.observableArrayList();
+                for (int i = 1; i <= rs.getMetaData().getColumnCount(); i++) {
+                    //Iterate Column
+                    row.add(rs.getString(i));
+                }
+                System.out.println("Row [1] added " + row);
+                data.add(row);
+
+            }
+
+            //FINALLY ADDED TO TableView
+            tblReports.setItems(data);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
 
